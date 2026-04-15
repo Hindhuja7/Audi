46957597ce587c275e6ce7991fbc2aa3db6fac9e
Hindhuja7 <lokinenihindhuja@gmail.com>
2026-04-15 16:32:42 +0530

    dsu

---
+++ DSU/dsu.java
@@ -0,0 +1,59 @@
+// // Program to implement Disjoint Set Data Structure.
+
+// /*
+// 5 = number of friends
+// 3 = number of relationships
+// 2 = number of friendship check
+// input=5
+// 3
+// 0 2
+// 4 2
+// 3 1
+// 2
+// 4 0
+// 1 0
+// output=4 is a friend of 0
+// 1 is not a friend of 0
+
+// */
+// import java.io.*;
+// import java.util.*;
+
+// class DisjointUnionSets {
+// 	int[] rank, parent;
+// 	int n;
+
+// 	public DisjointUnionSets(int n){
+// 		rank = new int[n];
+// 		parent = new int[n];
+// 		this.n = n;
+// 		makeSet();
+// 	}
+// 	// Creates n sets with single item in each
+// 	void makeSet(){
+		
+// 	}
+
+// 	// Returns representative of x's set
+// 	int find(int x){
+		
+// 	}
+
+// 	// Unites the set that includes x and the set that includes x
+// 	void union(int x, int y){
+	
+// 	}
+// }
+
+// class test {
+// 	public static void main(String[] args){
+// 		Scanner sc=new Scanner(System.in);
+		
+// 		}		
+// 	}
+// }
+
+
+public class dsu {
+    
+}
